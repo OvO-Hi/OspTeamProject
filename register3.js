260500fdda359e152a8488fc6e3aca786c2371b4
sngminlee98 <113418905+sngminlee98@users.noreply.github.com>
2022-11-05 11:19:54 +0900

    register.html
    맛집 등록, 대표 메뉴, 리뷰 등록 화면

---
+++ register3.js
@@ -0,0 +1,10 @@
+function PreviewImage() {
+  // 파일리더 생성 
+  var preview = new FileReader();
+  preview.onload = function (e) {
+  // img id 값 
+  document.getElementById("user_image").src = e.target.result;
+};
+// input id 값 
+preview.readAsDataURL(document.getElementById("user_profile_img").files[0]);
+};

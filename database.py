a50d56a37f865eb6f40388f82387cf154e763e19
gongkeo <112560299+gongkeo@users.noreply.github.com>
2022-11-17 15:07:45 +0900

    Add files via upload

---
+++ database.py
@@ -0,0 +1,104 @@
+import pyrebase
+import json
+
+class DBhandler:
+    def __init__(self):
+        with open('./authentication/firebase_auth.json') as f:
+            config=json.load(f)
+    
+        firebase = pyrebase.initialize_app(config)
+        self.db = firebase.database()
+    
+    #register1
+    def insert_restaurant(self, name, data, img_path):
+        restaurant_info ={
+            "res_addr": data['res_addr'],
+            "res_addr_detail": data['res_addr_detail'],
+            "res_tel": data['res_tel'],
+            "res_site": data['res_site'],
+            "res_area": data['res_area'],
+            "res_category": data['res_category'],
+            "res_price": data['res_price'],
+            "parking": data['parking'],
+            "appt_open": data['appt_open'],
+            "appt_close": data['appt_close'],
+            "img_path": img_path
+        }
+        if self.restaurant_duplicate_check(name):
+            self.db.child("restaurant").child(name).set(restaurant_info)
+            print(data, img_path)
+            return True
+        else:
+            return False
+    
+    def restaurant_duplicate_check(self, name):
+        restaurants = self.db.child("restaurant").get()
+        for res in restaurants.each():
+            if res.key() == name:
+                return False
+        return True
+    
+    #register2
+    def insert_menu(self, name, data, img_path):
+        menu_info = {
+            "img_path":img_path,
+            "res_name":data['res_name'],
+            "menu_name":data['menu_name'],
+            "menu_price":data['menu_price']
+        }
+        
+        #중복 확인
+        name = data['res_name'] + '_' + name
+        
+        if self.menu_duplicate_check(name):
+            self.db.child("menu").child(name).set(menu_info)
+            print(data, img_path)
+            return True
+        else:
+            return False
+    
+    def menu_duplicate_check(self, name):
+        menus = self.db.child("menu").get()
+        for menu in menus.each():
+            if menu.key() == name:
+                return False
+        return True
+    
+    
+    #register3
+    def insert_review(self, name, data, img_path):
+        review_info = {
+            "img_path": img_path,
+            "res_name": data['res_name'],
+            "rev_name": data['rev_name'],
+            "rev_menu": data['rev_menu'],
+            "rev_price": data['rev_price'],
+            "rev_score": data['rev_score'],
+            "rev_time": data['rev_time'],
+            "rev_memo": data['rev_memo'],
+            "rev_headcount": data['rev_headcount'],
+            "rev_amount": data['rev_amount'],
+            "rev_vegan": data['rev_vegan'],
+            "rev_etc": data['rev_etc'],
+            "img_path": img_path
+        }
+        
+        #중복 확인
+        name = data['res_name'] + '_' + name
+    
+        if self.review_duplicate_check(name):
+            self.db.child("review").child(name).set(review_info)
+            print(data, img_path)
+            return True
+        else:
+            return False
+    
+    def review_duplicate_check(self, name):
+        reviews = self.db.child("review").get()
+        for rev in reviews.each():
+            if rev.key() == name:
+                return False
+        return True
+    
+    
+        
